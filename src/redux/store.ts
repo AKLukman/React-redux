6681bd0d5285004f5e735cf25ee6d3a50c1f2e9c
AKLukman <abulkashemlukman@gmail.com>
2024-01-31 20:17:43 +0000

    rtk

---
+++ src/redux/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartSlice from './features/cart/cartSlice';
+import productSlice from './features/products/productSlice';
+import { apiSlice } from './api/apiSlice';
+
+const store = configureStore({
+  reducer: {
+    cart: cartSlice,
+    product: productSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
